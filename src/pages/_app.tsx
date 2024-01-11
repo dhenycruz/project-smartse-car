68b58a6cde45bc2f6341db3020591448193fdfb0
Dheniarley Cruz <dheniarley.ds@gmail.com>
2024-01-10 23:19:06 -0300

    pause

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
-import '@/styles/globals.css'
+import React from 'react'
 import type { AppProps } from 'next/app'
+import { SessionProvider, useSession } from 'next-auth/react'
+import Router from 'next/router'
+import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface AppPropsWithAuth extends AppProps {
+  Component: AppProps['Component'] & { auth: boolean }
 }
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppPropsWithAuth): React.ReactElement => {
+  return (
+    <SessionProvider session={ session }>
+      {
+        (Component?.auth)
+          ? (
+          <Auth>
+            <Component { ...pageProps } />
+          </Auth>
+            )
+          : (
+          <Component { ...pageProps } />
+            )
+      }
+    </SessionProvider>
+  )
+}
+
+const Auth = ({ children }: { children: React.ReactElement }): React.ReactElement => {
+  const { status } = useSession()
+
+  if (status === 'unauthenticated') void Router.replace('/')
+
+  if (status === 'loading') return (<div>Loading...</div>)
+
+  return children
+}
+
+export default App
