5961252f210bb9a19e3c253531bfe456a57c67d7
Dheniarley Cruz <dheniarley.ds@gmail.com>
2024-01-13 08:20:52 -0300

    fix: fix bug route api users

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,57 +1,57 @@
 import { PrismaClient } from '@prisma/client'
 import { type NextApiRequest, type NextApiResponse } from 'next'
 import bcrypt from 'bcryptjs'
-import { getToken } from 'next-auth/jwt'
+// import { getToken } from 'next-auth/jwt'
 
 const prisma = new PrismaClient()
-const secret = process.env.SECRET
+// const secret = process.env.SECRET
 
 export default async function handler (req: NextApiRequest, res: NextApiResponse): Promise<void> {
   const { method } = req
 
-  const token = await getToken({ req, secret })
+  /*   const token = await getToken({ req, secret })
   console.log(token)
-  if (token !== null) {
-    if (method === 'GET') {
-      const users = await prisma.user.findMany({
-        select: {
-          id: true,
-          name: true,
-          cpf: true,
-          email: true
-        }
-      })
+  if (token !== null) { */
+  if (method === 'GET') {
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        cpf: true,
+        email: true
+      }
+    })
 
-      res.status(200).json({ data: users })
-    } else if (method === 'POST') {
-      const { name, email, cpf, password } = req.body
+    res.status(200).json({ data: users })
+  } else if (method === 'POST') {
+    const { name, email, cpf, password } = req.body
 
-      const verifyCPF = await prisma.user.findUnique({
-        where: { cpf }
-      })
+    const verifyCPF = await prisma.user.findUnique({
+      where: { cpf }
+    })
 
-      if (verifyCPF != null) res.status(409).json({ message: 'CPF já cadastrado.' })
+    if (verifyCPF != null) res.status(409).json({ message: 'CPF já cadastrado.' })
 
-      const verifyEmail = await prisma.user.findUnique({
-        where: { email }
-      })
+    const verifyEmail = await prisma.user.findUnique({
+      where: { email }
+    })
 
-      if (verifyEmail != null) res.status(409).json({ message: 'Email já cadastrado.' })
+    if (verifyEmail != null) res.status(409).json({ message: 'Email já cadastrado.' })
 
-      const salt = await bcrypt.genSalt(10)
-      const newPassowrd = await bcrypt.hash(String(password), salt)
+    const salt = await bcrypt.genSalt(10)
+    const newPassowrd = await bcrypt.hash(String(password), salt)
 
-      const user = await prisma.user.create({
-        data: {
-          name, email, cpf, password: newPassowrd
-        }
-      })
+    const user = await prisma.user.create({
+      data: {
+        name, email, cpf, password: newPassowrd
+      }
+    })
 
-      res.status(201).json({ id: user.id, name: user.name, cpf: user.cpf })
-    }
-
-    res.status(404).json({ message: 'Route not found' })
+    res.status(201).json({ id: user.id, name: user.name, cpf: user.cpf })
   }
 
-  res.status(401).json({ message: 'unauthorized' })
+  res.status(404).json({ message: 'Route not found' })
 }
+
+// res.status(401).json({ message: 'unauthorized' })
+// }
